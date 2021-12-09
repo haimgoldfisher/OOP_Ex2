3b970a75666ffdbb3fc23530928e2d3f5d817be4
Or Yitshak <tur3103@gmail.com>
2021-12-09 11:30:18 +0200

    Pair class added

---
+++ src/Pair.java
@@ -0,0 +1,42 @@
+import java.util.Comparator;
+
+public class Pair implements Comparator<Pair> {
+    private int key;
+    private double distance;
+
+    public Pair() {
+    }
+
+    public Pair(int key, double distance){
+        this.key = key;
+        this.distance=distance;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public int compare(Pair nd1, Pair nd2) {
+        if(nd1.distance< nd2.distance){
+            return -1;
+        }
+        if (nd1.distance> nd2.distance){
+            return 1;
+        }
+
+        return 0;
+    }
+}
